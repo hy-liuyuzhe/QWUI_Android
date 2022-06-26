784e3906074892ade618ab105ddfb15c6b298f80
liuyuzhe <15313482685@163.com>
2022-06-26 16:00:53 +0800

    原画质生成视频
    Signed-off-by: liuyuzhe <15313482685@163.com>

--- app/src/main/java/com/qwuiteam/project/fragment/FFmpagFragment.kt
+++ app/src/main/java/com/qwuiteam/project/fragment/FFmpagFragment.kt
@@ -6,9 +6,12 @@ import android.view.View
 import com.arthenica.ffmpegkit.FFmpegKit
 import com.arthenica.ffmpegkit.FFprobeKit.getMediaInformation
 import com.arthenica.ffmpegkit.ReturnCode
+import com.blankj.utilcode.util.FileUtils
 import com.blankj.utilcode.util.LogUtils
 import com.qwuiteam.project.R
+import com.qwuiteam.project.utils.openFile
 import kotlinx.android.synthetic.main.fragment_ffmpag.*
+import java.io.File
 
 
 /**
@@ -20,12 +23,14 @@ class FFmpagFragment : BaseFragment() {
     val cover = "/storage/emulated/0/Download/Cover.jpg"
     val dir = "/storage/emulated/0/Download/"
     val dirDownload = "/storage/emulated/0/Download"
+    val videoDir = "/storage/emulated/0/Download/video"
     val opening = "/storage/emulated/0/Download/input.mp4"
     val episode = "/storage/emulated/0/Download/opening.mp4"
     val ending = "/storage/emulated/0/Download/result.mp4"
     val output = "/storage/emulated/0/Download/output2.mp4"
     val output1 = "/storage/emulated/0/Download/output1.mp4"
     val audio1 = "/storage/emulated/0/Download/audio1.mp3"
+    //I/ffmpeg-kit: : Video: mpeg4 (mp4v / 0x7634706D), yuv420p(tv, bt709/bt709/smpte170m), 1280x720 [SAR 1:1 DAR 16:9], 569 kb/s
     val sunshine = "/storage/emulated/0/Download/video/sunshine.mp4"
     val piano_quiet = "/storage/emulated/0/Download/audio/piano_quiet.mp3"
     val class_alarm = "/storage/emulated/0/Download/audio/class_alarm.mp3"
@@ -34,7 +39,48 @@ class FFmpagFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        maintain_original_quality.setOnClickListener {
+            /*val command = "-i $sunshine -vcodec libx265 -crf 28 $videoDir/maintain_original_quality2.mp4"
+            FFmpegKit.executeAsync(command) {
+                Log.d("wwq", "FFmpegSession: " + it.returnCode);
+
+                if (ReturnCode.isSuccess(it.returnCode)) {
+                    it.output.openFile(requireContext())
+                }
+            }*/
+            val out = "$videoDir/maintain_original_quality3.mp4"
+            val command = "-y -i $sunshine -acodec copy -vcodec libx264 $out"
+            FFmpegKit.executeAsync(command) {
+                if (ReturnCode.isSuccess(it.returnCode)) {
+                    out.openFile(requireContext())
+                }
+            }
+        }
         //./s/ffmpeg -loop 1 -i /files/v/smallest_bunny_1080p_30fps_001.jpg -c:v libx264 -pix_fmt yuv420p -t 10 /files/v/smallest_bunny_1080p_30fps_frame_001.mp4
+        addWatermark_music.setOnClickListener {
+            val commands = arrayListOf<String>()
+            val output1 = "$dirDownload/video/video_with_watermarkV2.mp4"
+            commands.apply {
+                add("-i")
+                add(sunshine)
+                add("-i")
+                add(piano_quiet)
+                add("-filter_complex")
+                add("overlay=20:20")
+                add(output1)
+            }
+            FFmpegKit.executeWithArguments(commands.toTypedArray())
+
+            val output = "$dir/video/addWatermark_music.mp4"
+            FileUtils.delete(output)
+            val command = "-i $output1 -i $piano_quiet -map 0:v -map 1:a -b:v 4455306k -s 2560x1440 -shortest $output"
+            FFmpegKit.executeAsync(command){
+                Log.d("wwq", "FFmpegSession: "+it.returnCode);
+                if(ReturnCode.isSuccess(it.returnCode)){
+                    output.openFile(requireContext())
+                }
+            }
+        }
         image_to_video.setOnClickListener {
             val output = "$dir/video/image_to_video.mp4"
             FFmpegKit.execute("-loop 1 -i $cover -t 3 $output")
@@ -57,7 +103,7 @@ class FFmpagFragment : BaseFragment() {
         //比特率 * 时间(s) = file size (kb) * 8
         //比特率 * duration /1024/1024 / 8 = file zie (mb)
         media_info.setOnClickListener {
-            val information = getMediaInformation(opening).mediaInformation
+            val information = getMediaInformation(sunshine).mediaInformation
             LogUtils.d("info: " + information.allProperties)
         }
 
@@ -104,8 +150,9 @@ class FFmpagFragment : BaseFragment() {
             }
         }
         video_convert_images.setOnClickListener {
+            File("$dirDownload/images").mkdir()
             FFmpegKit.execute(
-                "-i $opening -vf fps=1 $dir%d.png"
+                "-i $sunshine -vf fps=1 $dirDownload/images/%d.png"
             ).let {
                 if (ReturnCode.isSuccess(it.returnCode)) {
                     Log.d("wwq", "SUCCESS: ");
