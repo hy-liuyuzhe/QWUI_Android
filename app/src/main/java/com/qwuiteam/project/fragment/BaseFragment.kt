bcd3f08b824df40167ba095700a220f181f135d6
liuyuzhe <15313482685@163.com>
2022-07-15 11:56:54 +0800

    [feature_1.0.4.0] 1.expand layout
    Signed-off-by: liuyuzhe <15313482685@163.com>

--- app/src/main/java/com/qwuiteam/project/fragment/BaseFragment.kt
+++ app/src/main/java/com/qwuiteam/project/fragment/BaseFragment.kt
@@ -5,11 +5,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.noober.background.BackgroundLibrary
 
 abstract class BaseFragment : Fragment() {
 
     companion object {
          var TAG: String = this::class.simpleName.toString()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        BackgroundLibrary.inject(context)
     }
 
     override fun onCreateView(
---
+++ app/src/main/java/com/qwuiteam/project/view/RoundConstraintLayout.kt
@@ -0,0 +1,25 @@
+package com.qwuiteam.project.view
+
+import android.content.Context
+import android.graphics.Outline
+import android.util.AttributeSet
+import android.view.View
+import android.view.ViewOutlineProvider
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.blankj.utilcode.util.SizeUtils
+
+class RoundConstraintLayout @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null
+) : ConstraintLayout(context, attrs) {
+
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+        outlineProvider = object : ViewOutlineProvider() {
+            override fun getOutline(view: View, outline: Outline) {
+                outline.setRoundRect(0, 0, view.width, view.height, SizeUtils.dp2px(12f).toFloat())
+            }
+        }
+        clipToOutline = true
+    }
+}
