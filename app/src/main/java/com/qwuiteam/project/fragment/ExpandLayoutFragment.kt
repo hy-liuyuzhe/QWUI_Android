8ece2368c5573fa783fb1a42b9d058dfa664694a
liuyuzhe <15313482685@163.com>
2022-07-15 11:09:44 +0800

    [feature_1.0.4.0] 1.expand layout
    Signed-off-by: liuyuzhe <15313482685@163.com>

--- app/src/main/java/com/qwuiteam/project/fragment/ExpandLayoutFragment.kt
+++ app/src/main/java/com/qwuiteam/project/fragment/ExpandLayoutFragment.kt
@@ -2,13 +2,17 @@ package com.qwuiteam.project.fragment
 
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.cardview.widget.CardView
 import androidx.transition.AutoTransition
 import androidx.transition.TransitionManager
 import com.blankj.utilcode.util.*
 import com.qwuiteam.project.R
+import com.qwuiteam.project.gone
+import com.qwuiteam.project.utils.AutoExpandTransition
 import com.qwuiteam.project.view.TouchRootLayout
+import com.qwuiteam.project.visible
 import kotlinx.android.synthetic.main.fragment_layout.*
 import kotlinx.android.synthetic.main.fragment_string.*
 
@@ -22,9 +26,13 @@ class ExpandLayoutFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val cardView = view.findViewById<CardView>(R.id.base_cardview);
+        val cardView = view.findViewById<ViewGroup>(R.id.base_cardview);
         val arrow = view.findViewById<ImageView>(R.id.arrow_button);
         val hiddenView = view.findViewById<View>(R.id.hidden_view);
+        val imageBigBg = view.findViewById<View>(R.id.imageBigBg);
+        val imageExpandBg = view.findViewById<View>(R.id.imageExpandBg);
+        imageExpandBg.gone()
+        imageBigBg.visible()
         arrow.setOnClickListener {
 
             // If the CardView is already expanded, set its visibility
@@ -37,6 +45,8 @@ class ExpandLayoutFragment : BaseFragment() {
                 // Class to create a default transition.
                 TransitionManager.beginDelayedTransition(cardView, AutoTransition());
                 hiddenView.setVisibility(View.GONE);
+//                imageExpandBg.setVisibility(View.VISIBLE);
+//                imageBigBg.setVisibility(View.GONE);
                 arrow.setImageResource(R.drawable.ic_baseline_expand_more_24);
             }
 
@@ -44,18 +54,13 @@ class ExpandLayoutFragment : BaseFragment() {
             // to visible and change the expand more icon to expand less.
             else {
 
-                TransitionManager.beginDelayedTransition(
-                    cardView, AutoTransition ());
+                TransitionManager.beginDelayedTransition(cardView, AutoTransition());
+//                imageExpandBg.setVisibility(View.GONE);
+//                imageBigBg.setVisibility(View.VISIBLE);
                 hiddenView.setVisibility(View.VISIBLE);
                 arrow.setImageResource(R.drawable.ic_baseline_expand_less_24);
             }
         }
-
-        val touchLayout = view.findViewById<TouchRootLayout>(R.id.touchLayout)
-        val temp = view.findViewById<View>(R.id.temp)
-        temp.postDelayed({
-            touchLayout.maxHeight = temp.height
-        },0)
     }
 
 }
